951aa83808d00e993c676a97efa1381b73a9b046
Crystal Andre <candre02.2@gmail.com>
2024-09-19 15:49:23 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Services from "./pages/Services.js";
 import HigoDesigns from "./pages/HigoDesigns.js";
 import Payment from "./pages/Payment.js";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import chakraProvider element
 import { ChakraProvider } from "@chakra-ui/react";
 // import elements
@@ -21,7 +21,7 @@ function App() {
   return (
 
       <ChakraProvider>
-        <Router>
+        <Router basename="/Higo-Designs2">
           <div className="App">
             <NavBar />
             <Routes>
